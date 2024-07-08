a6f87040b263a91169cdb85f356dae56b9eb46a3
SergeAvgust <serge.avgustovski@gmail.com>
2024-07-08 18:46:46 +0300

    celery+FastAPI ready

--- app/worker/celery_app.py
+++ app/worker/celery_app.py
@@ -8,25 +8,26 @@ REDIS_PORT = int(os.getenv('REDIS_PORT', '6379'))
 REDIS_USERNAME = os.getenv('REDIS_USERNAME', '')
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'password')
 
+"""
 AMQP_HOST = os.getenv('AMQP_HOST', 'localhost')
 AMQP_PORT = int(os.getenv('AMQP_PORT', '5672'))
 AMQP_USERNAME = os.getenv('AMQP_USERNAME', 'user')
 AMQP_PASSWORD = os.getenv('AMQP_PASSWORD', 'password')
-
+"""
 
 celery_app = Celery(
     'worker',
-    backend='redis://%s:%s@%s:%d/0' % (
+    broker='redis://%s:%s@%s:%d/0' % (
         REDIS_USERNAME,
         REDIS_PASSWORD,
         REDIS_HOST,
         REDIS_PORT
     ),
-    broker='amqp://%s:%s@%s:%d//' % (
-        AMQP_USERNAME,
-        AMQP_PASSWORD,
-        AMQP_HOST,
-        AMQP_PORT
+    backend='redis://%s:%s@%s:%d/0' % (
+        REDIS_USERNAME,
+        REDIS_PASSWORD,
+        REDIS_HOST,
+        REDIS_PORT
     )
 )
 celery_app.conf.task_routes = {
--- app/worker/celery_worker.py
+++ app/worker/celery_worker.py
@@ -9,6 +9,7 @@ from .celery_app import celery_app
 def test_celery(word: str) -> str:
     for i in range(1, 11):
         sleep(1)
+        print('Celery!')
         current_task.update_state(state='PROGRESS',
                                   meta={'process_percent': i*10})
     return f"test task return {word}"
