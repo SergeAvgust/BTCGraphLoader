5542e2292db4ed016d342fad597c68cc72a45a2f
SergeAvgust <serge.avgustovski@gmail.com>
2024-07-09 01:39:05 +0300

    Cut unnecessary view

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import logging
 
 from fastapi import FastAPI, BackgroundTasks
 
-from app.worker.celery_app import celery_app
 from app.api.routes import router
 
 log = logging.getLogger(__name__)
@@ -11,20 +10,3 @@ app = FastAPI()
 
 app.include_router(router)
 
-def celery_on_message(body):
-    log.warning(body)
-
-
-def background_on_message(task):
-    log.warning(task.get(on_message=celery_on_message, propagate=False))
-
-
-@app.get('/{word}')
-async def root(word: str, background_task: BackgroundTasks):
-    task_name = 'app.worker.celery_worker.test_celery'
-
-    task = celery_app.send_task(task_name, args=[word])
-    background_task.add_task(background_on_message, task)
-    print(task)
-
-    return {'message': 'Word received'}
